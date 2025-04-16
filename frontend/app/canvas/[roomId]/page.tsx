03fa9abaf388393da99b05c461ec5e702755c629
Shivam1983 <143506840+Shivam1983@users.noreply.github.com>
2025-04-16 22:32:30 +0530

    canvas changed

--- frontend/app/canvas/[roomId]/page.tsx
+++ frontend/app/canvas/[roomId]/page.tsx
@@ -1,11 +1,10 @@
-"use client";
-import { RoomCanvas } from "@/components/RoomCanvas";
-import React from "react";
+"use client"; 
+import { RoomCanvas } from "@/components/RoomCanvas"; // Importing the RoomCanvas component
+import React from "react"; // Importing React
 
+// This component represents a dynamic canvas page for a specific room
 export default function CanvasPage({ params }: { params: Promise<{ roomId: string}> }) {
-    const { roomId } = React.use(params);
-
-    if (!roomId) return <p>Loading...</p>;
-
-    return <RoomCanvas roomId={roomId} />;
+    const { roomId } = React.use(params); 
+    if (!roomId) return <p>Loading...</p>; 
+    return <RoomCanvas roomId={roomId} />; 
 }
